4e03cb2aa5feb7fb7097185302000003249db5ce
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-08-29 22:29:27 +0530

    build signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import './App.css'
+import {Loader} from 'lucide-react'
 
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -11,26 +12,43 @@ import Signup from './pages/Signup';
 import { useAuthStore } from './store/useAuth.store';
 
 function App() {
- const {authUser, checkAuth} = useAuthStore();
+ const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
 
  useEffect(() => {
    checkAuth();
  }, [checkAuth]);
 
- console.log(authUser);
+ console.log({authUser});
+ 
+//  if(!isCheckingAuth && !authUser  ) return (
+//     <div className="flex items-center justify-center h-screen">
+//       <Loader className="size-10 animate-spin"></Loader>
+  
+//   </div>
+//  )
+
+
 
   return (
     <>
      <div className="">
       <Navbar/>
-     <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/signup" element={<Signup/>}></Route>
-      <Route path="/login" element={<Login/>}></Route>
+       <Routes>
+      <Route path="/" element={ <Home/> }></Route>
+      <Route path="/signup" element={<Signup/> }></Route>
+      <Route path="/login" element={ <Login/> }></Route>
       <Route path="/setting" element={<Setting/>}></Route>
-      <Route path="/profile" element={<Profile/>}></Route>
+      <Route path="/profile" element={ <Profile/> }></Route>
 
      </Routes>
+     {/* <Routes>
+      <Route path="/" element={authUser  ? <Home/> : <Navigate to="/login" />}></Route>
+      <Route path="/signup" element={authUser  ? <Signup/> : <Navigate to="/" />}></Route>
+      <Route path="/login" element={authUser  ? <Login/> : <Navigate to="/" />}></Route>
+      <Route path="/setting" element={<Setting/>}></Route>
+      <Route path="/profile" element={authUser  ? <Profile/> : <Navigate to="/login" />}></Route>
+
+     </Routes> */}
      </div>
        
     </>
