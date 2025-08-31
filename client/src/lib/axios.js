0edcc7bf21ac0e9b8ee26f300f6faa959544d5f3
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-09-01 01:19:01 +0530

    project completed

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:5200/api",
+    baseURL : import.meta.env.MODE === "development" ? "http://localhost:5200/api" : "/api",
     withCredentials: true,
 })
