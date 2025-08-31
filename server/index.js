4ed91dd859b81feceb8cf4d0ae23319bf011227b
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-09-01 01:49:07 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ['http://localhost:5173', 
+      'https://pingmechatapp.onrender.com'],
     credentials: true,
 }))
 
